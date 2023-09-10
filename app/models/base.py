f22f8173e6abe1be9ec63ef8752573fd827d1cc5
Egor <egor29302@yandex.ru>
2023-09-10 12:37:34 +0300

    first commit

---
+++ app/models/base.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, Integer
+
+from app.core.db import Base
+
+
+class BaseModel(Base):
+    full_amount = Column(Integer, nullable=False)
+    invested_amount = Column(Integer, default=0)
+    fully_invested = Column(Boolean, default=False)
+    create_date = Column(DateTime, default=datetime.now)
+    close_date = Column(DateTime, default=None)
